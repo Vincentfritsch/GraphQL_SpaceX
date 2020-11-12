0d0fa410c8093c16ca81c946bef7c2191d7a24d7
Fritsch <v.fritsch1981@gmail.com>
2020-11-12 10:28:29 +0100

    First commit project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query GetLaunches {
+        launches(limit: 5) {
+          launch_date_utc
+          launch_success
+          rocket {
+            rocket_name
+          }
+          links {
+            video_link
+          }
+          details
+        }
+      }`
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
